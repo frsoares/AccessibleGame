bb66864328bee2660cedf9405ef1a9100d3e8540
Francisco Soares <fmssn@cin.ufpe.br>
2023-08-08 23:01:24 -0300

    Raquete de jogo básica com detalhe pra voice over
    A partir de uma simplificação do template padrão para jogos, foi
    adicionada uma "raquete" tal qual de um jogo de Breakout ou ping pong.
    
    Após isso, foi adicionada uma opção de código para poder lidar com a
    possibilidade do Voice Over estar ligado.
    
    Comentários em GameViewController apontam uma alternativa de uso.

--- AccessibleGame/GameViewController.swift
+++ AccessibleGame/GameViewController.swift
@@ -13,21 +13,33 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
+
+        if let view = self.view as? SKView {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
-                
+
                 // Present the scene
                 view.presentScene(scene)
             }
-            
+
             view.ignoresSiblingOrder = true
-            
+
             view.showsFPS = true
             view.showsNodeCount = true
+            /*
+             // se você descomentar este bloco de código e o voice over estiver ligado,
+             // a cena toda fica com interação direta, mas perde uma indicação do local
+             // da "raquete". O local pode ser importante! Neste exemplo, por exemplo,
+             // é interessante ter indicação do local da raquete porque é preciso tocar
+             // nela para movê-la.
+
+            if UIAccessibility.isVoiceOverRunning {
+                self.view.isAccessibilityElement = true
+                self.view.accessibilityTraits = .allowsDirectInteraction
+            }
+             */
         }
     }
 
